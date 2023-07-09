54cdb0db1ed7d69c7b98824bc3bf5f9c2a011a13
mgDentist <gmi1987@gmail.com>
2023-07-09 19:52:33 +0400

    whelp of teacher

--- js/gallery.js
+++ js/gallery.js
@@ -1,103 +1,52 @@
-// Функция для открытия полноразмерного изображения
+import { clearComments, renderComments } from './comments.js';
+
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+
+const socialCaption = bigPicture.querySelector('.social__caption');
+
+const body = document.body;
+const closeButton = bigPicture.querySelector('#picture-cancel');
+
+
+/**  Функция для закрытия окна по нажатию клавиши Esc */
+const onDocumentKeyDown = (evt) => {
+	if (evt.key === 'Escape') {
+		closeGallery();
+	}
+};
+
+/**  Функция для закрытия окна при клике по иконке закрытия */
+const onCloseButtonClick = closeGallery;
+
+/** Функция для закрытия окна */
+function closeGallery() {
+	bigPicture.classList.add('hidden');
+	body.classList.remove('modal-open');
+	clearComments();
+	document.removeEventListener('keydown', onDocumentKeyDown);
+	closeButton.removeEventListener('click', onCloseButtonClick);
+}
+
+/** Функция для открытия полноразмерного изображения */
 const openGallery = (picture) => {
-	const bigPicture = document.querySelector('.big-picture');
-	const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
-	const likesCount = bigPicture.querySelector('.likes-count');
-	const commentsCount = bigPicture.querySelector('.comments-count');
-	const socialCaption = bigPicture.querySelector('.social__caption');
-	const socialComments = bigPicture.querySelector('.social__comments');
-	const body = document.body;
 
 	bigPictureImg.src = picture.url;
 	likesCount.textContent = picture.likes;
-	commentsCount.textContent = picture.comments.length;
+
 	socialCaption.textContent = picture.description;
-
-	// Удаление всех комментариев из списка
-	while (socialComments.firstChild) {
-		socialComments.removeChild(socialComments.firstChild);
-	}
-
-	// Добавление комментариев в список
-	picture.comments.forEach((comment) => {
-		const commentElement = document.createElement('li');
-		commentElement.classList.add('social__comment');
-
-		const commentPicture = document.createElement('img');
-		commentPicture.classList.add('social__picture');
-		commentPicture.src = comment.avatar;
-		commentPicture.alt = comment.name;
-		commentPicture.width = 35;
-		commentPicture.height = 35;
-
-		const commentText = document.createElement('p');
-		commentText.classList.add('social__text');
-		commentText.textContent = comment.message;
-
-		commentElement.appendChild(commentPicture);
-		commentElement.appendChild(commentText);
-		socialComments.appendChild(commentElement);
-	});
 
 	// Удаление класса hidden у элемента .big-picture
 	bigPicture.classList.remove('hidden');
-
 	// Добавление класса modal-open к элементу body
 	body.classList.add('modal-open');
 
-	// Функция для закрытия окна
-	const closeGallery = () => {
-		bigPicture.classList.add('hidden');
-		body.classList.remove('modal-open');
-		document.removeEventListener('keydown', onDocumentKeyDown);
-		closeButton.removeEventListener('click', onCloseButtonClick);
-	};
-
-	// Функция для закрытия окна по нажатию клавиши Esc
-	const onDocumentKeyDown = (evt) => {
-		if (evt.key === 'Escape') {
-			closeGallery();
-		}
-	};
-
-	// Функция для закрытия окна при клике по иконке закрытия
-	const onCloseButtonClick = () => {
-		closeGallery();
-	};
+	renderComments(picture.comments);
 
 	// Закрытие окна по нажатию клавиши Esc
 	document.addEventListener('keydown', onDocumentKeyDown);
-
-	// Закрытие окна при клике по иконке закрытия
-	const closeButton = bigPicture.querySelector('#picture-cancel');
 	closeButton.addEventListener('click', onCloseButtonClick);
 };
 
 export { openGallery };
-
-
-// import { renderThumbnails } from './thumbnail.js';
-// import { showBigPicture } from './big-picture.js'
-
-// const container = document.querySelector('.pictures');
-
-// const renderGallery = (pictures) => {
-// 	container.addEventListener('click', (evt) => {
-// 		const thumbnail = evt.target.closest('[data-thumbnail-id');
-// 		if (!thumbnail) {
-// 			return;
-// 		}
-
-// 		evt.preventDefault();
-// 		const picture = pictures.find(
-// 			(item) => item.id === +thumbnail.dataset.thumbnailId
-// 		);
-// 		showBigPicture(picture);
-// 	});
-
-// 	renderThumbnails(pictures, container);
-// };
-
-// renderGallery();
-
-// export { renderGallery };
--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,4 +1,3 @@
-import { getPictures } from './data.js';
 import { openGallery } from './gallery.js';
 
 const picturesContainer = document.querySelector('.pictures');
@@ -7,21 +6,23 @@ const pictureTemplate = document.querySelector('#picture')?.content.querySelecto
 // Функция для отрисовки миниатюры фотографии
 const renderThumbnail = (picture) => {
 	const pictureLink = pictureTemplate.cloneNode(true);
-	const pictureImg = pictureElement.querySelector('.picture__img');
-	const pictureLikes = pictureElement.querySelector('.picture__likes');
-	const pictureComments = pictureElement.querySelector('.picture__comments');
+	const pictureImg = pictureLink.querySelector('.picture__img');
+	const pictureLikes = pictureLink.querySelector('.picture__likes');
+	const pictureComments = pictureLink.querySelector('.picture__comments');
 
-	pictureImg.src = picture.url;
-	pictureImg.alt = picture.description;
-	pictureLikes.textContent = picture.likes;
-	pictureComments.textContent = picture.comments.length;
+	const {url, description, likes, comments} = picture;
+
+	pictureImg.src = url;
+	pictureImg.alt = description;
+	pictureLikes.textContent = likes;
+	pictureComments.textContent = comments.length;
 
 	pictureLink.addEventListener('click', (evt) => {
 		evt.preventDefault();
 		openGallery(picture);
 	});
 
-	return pictureElement;
+	return pictureLink;
 };
 
 /** Функция для отрисовки миниатюр всех фотографий */
@@ -36,40 +37,4 @@ const renderThumbnails = (pictures) => {
 	picturesContainer.appendChild(fragment);
 };
 
-// Получаем данные фотографий и отрисовываем миниатюры
-const pictures = getPictures();
-renderThumbnails(pictures);
-
 export { renderThumbnails };
-
-
-// const thumbnailTemplate = document
-// 	.querySelector('#picture').content
-// 	.querySelector('.picture');
-
-// const container = document.querySelector('.pictures');
-
-// const createThumbnail = ({ id, comments, description, likes, url }) => {
-// 	const thumbnail = thumbnailTemplate.cloneNode(true);
-
-// 	thumbnail.querySelector('.picture__img').src = url;
-// 	thumbnail.querySelector('.picture__img').alt = description;
-// 	thumbnail.querySelector('.picture__comments').textContent = comments.length;
-// 	thumbnail.querySelector('.picture__likes').textContent = likes;
-
-// 	thumbnail.dataset.thumbnailId = id; // выдает ошибку
-
-// 	return thumbnail;
-// };
-
-// const renderThumbnails = (pictures) => {
-// 	const fragment = document.createDocumentFragment();
-// 	pictures.forEach((picture) => {
-// 		const thumbnail = createThumbnail(picture);
-// 		fragment.append(thumbnail);
-// 	});
-
-// 	container.append(fragment);
-// };
-
-// export { renderThumbnails };
