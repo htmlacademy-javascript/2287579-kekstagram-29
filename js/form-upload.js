4f79086877f55c8f19e4dd7ca03600866ad90861
mgDentist <gmi1987@gmail.com>
2023-07-21 11:55:51 +0400

    Добавляет message.js

--- js/form-upload.js
+++ js/form-upload.js
@@ -44,3 +44,5 @@ function onDocumentKeyDown(evt) {
 		}
 	});
 });
+
+export { closeModal };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 import { renderThumbnails } from './thumbnail.js';
 import { getPictures } from './data.js';
 import './form-upload.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { closeModal } from './form-upload.js';
+import { showErrorMessage, showSuccessMessage } from './message.js';
 
 const pictures = getPictures();
 renderThumbnails(pictures);
+
+setOnFormSubmit(async (data) => {
+	try {
+		await sendData(data);
+		closeModal(); //не уверен
+		showSuccessMessage();
+	} catch {
+		showErrorMessage();
+	}
+});
+
+try {
+	const data = await getData();
+	renderThumbnails(data); //не уверен
+} catch (err) {
+	showAlert(err.message);
+}
