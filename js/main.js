70b7b1852b155066be110acd897d4a7c0da0a738
mgDentist <gmi1987@gmail.com>
2023-07-23 15:03:03 +0400

    Добавляет изменения main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import { renderThumbnails } from './thumbnail.js';
 import { getPictures } from './data.js';
 import './form-upload.js';
 import { getData, sendData } from './api.js';
-import { showAlert } from './util.js';
+import { debounce, showAlert } from './util.js';
 import { closeModal } from './form-upload.js';
 import { showErrorMessage, showSuccessMessage } from './message.js';
 import { setOnFormSubmit } from './form-validation.js';
+import { initFilter, getFilteredPictures } from './filter.js';
 
 const pictures = getPictures();
 renderThumbnails(pictures);
@@ -22,7 +23,9 @@ setOnFormSubmit(async (data) => {
 
 try {
 	const data = await getData();
-	renderThumbnails(data); //не уверен
+	const debouncedRenderGallery = debounce(renderThumbnails);
+	initFilter(data, debouncedRenderGallery);
+	renderThumbnails(getFilteredPictures); //не уверен
 } catch (err) {
 	showAlert(err.message);
 }
