8e4165025a1e3c5f3739748fb597c999c01ff49b
mgDentist <gmi1987@gmail.com>
2023-07-24 12:54:42 +0400

    Написана функция подстановки фото в form-validation, но не работает

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,15 +1,37 @@
 import { isUniqueArr } from './data.js';
+import { startUpload } from './form-upload.js';
 
 const SubmitButtonText = {
 	IDLE: 'Опубликовать',
 	SUBMITTING: 'Отправляю...',
 };
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const form = document.querySelector('.img-upload__form');
 const inputHashtag = form.querySelector('input[name="hashtags"]');
 const submitButton = form.querySelector('.img-upload__submit');
+const fileField = form.querySelector('.img-upload__input');
+const photoPreview = form.querySelector('.img-upload__preview img');
+const effectsPrewiev = form.querySelector('.effects__preview ');
 
 let hashtagArray = [];
+
+const isValidType = (file) => {
+	const fileName = file.name.toLowerCase();
+	return FILE_TYPES.some((it) => fileName.endsWith(it));
+};
+
+const onFileInputChange = () => {
+	const file = fileField.files[0];
+
+	if (file && isValidType(file)) {
+		photoPreview.src = URL.createObjectURL(file);
+		effectsPrewiev.forEach((preview) => {
+			preview.style.backgroundImage = `url('${photoPreview.src}')`;
+		});
+	}
+	startUpload();
+};
 
 const pristine = new Pristine(form, {
 	classTo: 'img-upload__field-wrapper',
